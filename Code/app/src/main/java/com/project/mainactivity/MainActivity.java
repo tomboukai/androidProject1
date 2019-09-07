750b4d8bb04d3f181be3486e6d4cce6169e26d64
netef <38144727+netef@users.noreply.github.com>
2019-09-07 14:45:58 +0300

    added permissions

--- Code/app/src/main/java/com/project/mainactivity/MainActivity.java
+++ Code/app/src/main/java/com/project/mainactivity/MainActivity.java
@@ -3,9 +3,14 @@
 
 package com.project.mainactivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -14,14 +19,24 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //location
+    private final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final String FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private Location userLocation;
+    private boolean mLocationPermissionsGranted = false;
+
+
     private EditText nameEditText;
     private ImageView playBtn;
     private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getGoogleMapsPermissions();
 
         sharedPreferences = getSharedPreferences("com.project.mainactivity", MODE_PRIVATE);
         nameEditText = findViewById(R.id.nameEditText);
@@ -39,5 +54,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void getGoogleMapsPermissions() {
+        String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
     }
 }
